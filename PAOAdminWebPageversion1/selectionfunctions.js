d7867f26c220ff3a27e7ad7c12094d6c5705d78a
Jenrish Chan <2014069138@ust-ics.mygbiz.com>
2016-05-11 18:04:04 +0800

    Big Update

---
+++ PAOAdminWebPageversion1/selectionfunctions.js
@@ -0,0 +1,88 @@
+$(document).ready(function(){
+	$("#mail").click(function(){
+		window.location ="mail.html";
+		
+	});
+  });
+
+$(document).ready(function(){
+	$("#submissions").click(function(){
+		window.location ="SubmissionsMain.html";
+		
+	});
+  });
+
+
+$(document).ready(function(){
+	$("#forms").click(function(){
+		window.location ="FormsMain.html";
+		
+	});
+  });
+
+
+$(document).ready(function(){
+	$("#requests").click(function(){
+		window.location ="RequestsMain.html";
+		
+	});
+  });
+
+
+$(document).ready(function(){
+	$("#update").click(function(){
+		window.location ="UpdateMain.html";
+		
+	});
+  });
+
+
+
+$(document).ready(function(){
+	$("#mail").click(function(){
+		window.location ="mail.html";
+		
+	});
+  });
+
+$(document).ready(function(){
+	
+	$("#logout").click(function(){
+		if(confirm("Do you want to logout? ")==true){
+			window.location="AdminLoginPage.html";
+		}
+		else{
+
+		}
+		
+		
+  });
+
+});
+	
+
+$(document).ready(function(){
+
+		$("#news").click(function(){
+		window.location="";
+		});
+
+
+	});
+
+$(document).ready(function(){
+		$("#home").click(function(){
+			window.location="Selection.html";
+
+		});
+
+
+});
+
+$(document).ready(function(){
+		$("#back").click(function(){
+			window.location="";
+
+		});
+});
+	

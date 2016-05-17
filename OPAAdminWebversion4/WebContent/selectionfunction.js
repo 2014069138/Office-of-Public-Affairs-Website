04ee1f00538123358e1c692f8016afeba30c2dcb
Jenrish Chan <2014069138@ust-ics.mygbiz.com>
2016-05-17 22:36:59 +0800

    Version 4
    Final Big Update

---
+++ OPAAdminWebversion4/WebContent/selectionfunction.js
@@ -0,0 +1,235 @@
+/**
+ * 
+ */
+
+$(document).ready(function(){
+	$("#profpic").dblclick(function(){
+		$("#profilemenu").slideDown();
+	});
+});
+
+$(document).ready(function(){
+	$("#profpic").click(function(){
+		$("#profilemenu").slideUp();
+	});
+});
+
+$(document).ready(function(){
+	$("#logout").click(function(){
+		if(confirm("Do you want to logout? ")==true){
+			window.location="AdminLoginPage.html";
+		}
+		else{
+
+		}
+	});
+});
+/* Mail */
+$(document).ready(function(){
+	$("#feedback").click(function(){
+		$("#inbox2").slideUp();
+		$("#inbox").slideDown();
+		
+	});
+});
+
+$(document).ready(function(){
+	$("#feedback").dblclick(function(){
+		$("#inbox").slideUp();
+	});
+});
+
+$(document).ready(function(){
+	$("#email").click(function(){
+		$("#inbox").slideUp();
+		$("#inbox2").slideDown();
+		
+	});
+});
+
+$(document).ready(function(){
+	$("#email").dblclick(function(){
+		$("#inbox2").slideUp();
+	});
+});
+
+/*Submissions*/
+$(document).ready(function(){
+	$("#ads").click(function(){
+		$("#inbox2").slideUp();
+		$("#inbox").slideDown();
+		
+	});
+});
+
+$(document).ready(function(){
+	$("#ads").dblclick(function(){
+		$("#inbox").slideUp();
+	});
+});
+
+$(document).ready(function(){
+	$("#news").click(function(){
+		$("#inbox").slideUp();
+		$("#inbox2").slideDown();
+		
+	});
+});
+
+$(document).ready(function(){
+	$("#news").dblclick(function(){
+		$("#inbox2").slideUp();
+	});
+});
+
+/*Requests*/
+$(document).ready(function(){
+	$("#tokens").click(function(){
+		$("#inbox3").slideUp();
+		$("#inbox2").slideUp();
+		$("#inbox").slideDown();
+		
+	});
+});
+
+$(document).ready(function(){
+	$("#tokens").dblclick(function(){
+		$("#inbox").slideUp();
+	});
+});
+
+$(document).ready(function(){
+	$("#materials").click(function(){
+		$("#inbox").slideUp();
+		$("#inbox3").slideUp();
+		$("#inbox2").slideDown();
+		
+	});
+});
+
+$(document).ready(function(){
+	$("#materials").dblclick(function(){
+		$("#inbox2").slideUp();
+	});
+});
+
+$(document).ready(function(){
+	$("#info").click(function(){
+		$("#inbox").slideUp();
+		$("#inbox2").slideUp();
+		$("#inbox3").slideDown();
+	
+	});
+});
+
+$(document).ready(function(){
+	$("info").dblclick(function(){
+		$("inbox3").slideDown();
+	});
+});
+
+/*Forms*/
+
+$(document).ready(function(){
+	$("#cover").click(function(){
+		$("#inbox4").slideUp();
+		$("#inbox3").slideUp();
+		$("#inbox2").slideUp();
+		$("#inbox").slideDown();
+		
+	});
+});
+
+$(document).ready(function(){
+	$("#cover").dblclick(function(){
+		$("#inbox").slideUp();
+	});
+});
+
+$(document).ready(function(){
+	$("#visitors").click(function(){
+		$("#inbox4").slideUp();
+		$("#inbox").slideUp();
+		$("#inbox3").slideUp();
+		$("#inbox2").slideDown();
+		
+	});
+});
+
+$(document).ready(function(){
+	$("#visitors").dblclick(function(){
+		$("#inbox2").slideUp();
+	});
+});
+
+$(document).ready(function(){
+	$("#shoots").click(function(){
+		$("#inbox4").slideUp();
+		$("#inbox").slideUp();
+		$("#inbox2").slideUp();
+		$("#inbox3").slideDown();
+	
+	});
+});
+
+$(document).ready(function(){
+	$("#shoots").dblclick(function(){
+		$("#inbox3").slideDown();
+	});
+});
+
+
+$(document).ready(function(){
+	$("#proposals").click(function(){
+		$("#inbox3").slideUp();
+		$("#inbox").slideUp();
+		$("#inbox2").slideUp();
+		$("#inbox4").slideDown();
+	});
+});
+
+
+$(document).ready(function(){
+	$("#proposals").dblclick(function(){
+		$("#inbox4").slideUp();
+	});
+});
+
+
+/*Udpdates*/
+
+$(document).ready(function(){
+	$("#events").click(function(){
+		$("#calendarbox").slideUp();
+		$("#resourcebox").slideUp();
+		$("#gallerybox").slideUp();
+		$("#eventbox").slideDown();
+	});
+});
+
+$(document).ready(function(){
+	$("#calendar").click(function(){
+		$("#resourcebox").slideUp();
+		$("#eventbox").slideUp();
+		$("#gallerybox").slideUp();
+		$("#calendarbox").slideDown();
+	});
+});
+
+$(document).ready(function(){
+	$("#resources").click(function(){
+		$("#calendarbox").slideUp();
+		$("#eventbox").slideUp();
+		$("#gallerybox").slideUp();
+		$("#resourcebox").slideDown();
+	});
+});
+
+$(document).ready(function(){
+	$("#gallery").click(function(){
+		$("#resourcebox").slideUp();
+		$("#calendarbox").slideUp();
+		$("#eventbox").slideUp();
+		$("#gallerybox").slideDown();
+	});
+});
---
+++ OPAAdminWebversion4/src/LoginInfo.java
@@ -0,0 +1,45 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/LoginInfo")
+public class LoginInfo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+		String user ;
+		String pass;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request,response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		RequestDispatcher rd ;
+		
+		user = request.getParameter("username");
+		pass = request.getParameter("password");
+		
+		if(user.equals("AilexVillamor")&&pass.equals("admin1234")){
+			rd = request.getRequestDispatcher("index.html");
+			rd.forward(request,response);
+		}
+		else{
+			rd = request.getRequestDispatcher("AdminLoginPage.html");
+			out.println("Username and Password Invalid");
+			rd.include(request, response);
+		}
+		
+		out.close();
+	}
+
+}
